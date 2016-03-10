dbc02ebc99181acba54ccbf325ef99141728c923
chendong <chendong@host.(none)>
2016-03-10 17:36:52 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-#  Hello today 
+#  Hello today aaaaaaaaaaaa
 
--- expect.py
+++ expect.py
@@ -3,12 +3,11 @@ import pexpect
 import os
 
 os.chdir('/home/chendong/5/chendong2016.github.io')
-'''
 os.system('git pull')
 os.system('git add .')
 os.system('git commit -m update')
-'''
 
+print 'dddddddddddd'
 #foo = pexpect.spawn('scp -P 29999 body.txt root@144.168.61.108:/root/')
 foo = pexpect.spawn('git push')
 foo.expect('.*Username.*:')
